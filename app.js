e1940f99845ca0b091bb3fc51b211e5d7635d0f7
NuclMe <saltansergei@gmail.com>
2026-02-09 00:54:15 +0200

    added database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
+const db = require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -22,3 +22,4 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 app.listen(3000);
+
