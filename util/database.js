cc5bf1edc77d3507c037e34be8762e96e2e31774
NuclMe <saltansergei@gmail.com>
2026-03-04 10:13:39 +0200

    feat: deleted database link

--- util/database.js
+++ util/database.js
@@ -4,9 +4,7 @@ const MongoClient = mongodb.MongoClient;
 let _db;
 
 const mongoConnect = (callback) => {
-  MongoClient.connect(
-    'mongodb+srv://nodeAdmin:v4F39YpLtM4vy3XE@cluster0.1k4hqk4.mongodb.net/?appName=Cluster0',
-  )
+  MongoClient.connect('test')
     .then((client) => {
       console.log('Connected!');
       _db = client.db();
@@ -27,4 +25,3 @@ const getDb = () => {
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
-
